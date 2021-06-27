d9cecc4d1fb8f68c9052d0e4427da1f96e5e8d70
Waislan Sanches <waislanluis@gmail.com>
2021-06-27 19:25:00 -0300

    second class

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
+import "reflect-metadata";
 import express from 'express';
+import { router } from "./routes";
+
+import './database';
 
 const app = express();
 
-app.get('/test', (request, response) => {
-  return response.send('Hello World');
-})
-
-app.post('/test-post', (request, response) => {
-  return response.send('Hello World by POST method');
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running'));
